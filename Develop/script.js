06691e7c521afedfb19b4d7b0ff854c476f946e5
Bradley Troll <bradleytroll@gmail.com>
2024-01-28 11:30:05 -0500

    Adds functions to grab current date and hour using dayjs.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,21 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
+  
+  //Grabs current date
+  var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  $('#currentDay').text(currentDate);
+
+  //Grabs current hour
+  var currentHour = dayjs().hour();
+
+  console.log(currentDate)
+  console.log(currentHour)
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +35,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
